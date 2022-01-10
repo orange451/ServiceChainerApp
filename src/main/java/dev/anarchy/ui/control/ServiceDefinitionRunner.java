5d259b7af9c9853b1253990d0033f3aae3ebb57a
Andrew Hamilton <ahamilton@manh.com>
2022-01-09 23:54:37 -0500

    Fix error when null template is provided to test service definition template

--- src/main/java/dev/anarchy/ui/control/ServiceDefinitionRunner.java
+++ src/main/java/dev/anarchy/ui/control/ServiceDefinitionRunner.java
@@ -2,6 +2,8 @@ package dev.anarchy.ui.control;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+
 import dev.anarchy.ace.Modes;
 import dev.anarchy.common.DServiceDefinition;
 import dev.anarchy.translate.runner.BasicServiceChainRunner;
@@ -23,8 +25,12 @@ public class ServiceDefinitionRunner extends RunnerWindowBase {
 	@Override
 	protected void onTest() {
 		try {
-			serviceDefinition.setLastInput(code.getText());
-			Map<String, Object> inputPayload = JSONUtils.jsonToMap(code.getText());
+			String template = code.getText();
+			if ( StringUtils.isEmpty(template) )
+				template = "{}";
+			
+			serviceDefinition.setLastInput(template);
+			Map<String, Object> inputPayload = JSONUtils.jsonToMap(template);
 			Map<String, Object> result = new BasicServiceChainRunner(null).transformSingle(serviceDefinition, inputPayload, false);
 			String prettyResult = JSONUtils.mapToJsonPretty(result);
 			addResult("Test Result [ok]", prettyResult);
