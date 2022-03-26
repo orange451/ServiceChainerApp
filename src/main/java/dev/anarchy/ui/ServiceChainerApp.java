69e37f472ec3623e255b613de44d47f09fa58d6f
Andrew Hamilton <ahamilton@manh.com>
2022-03-26 18:55:32 -0400

    Improve File Deletion

--- src/main/java/dev/anarchy/ui/ServiceChainerApp.java
+++ src/main/java/dev/anarchy/ui/ServiceChainerApp.java
@@ -1,6 +1,9 @@
 package dev.anarchy.ui;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
+
 import dev.anarchy.common.DApp;
 import dev.anarchy.common.DCollection;
 import dev.anarchy.common.DFolder;
@@ -10,9 +13,13 @@ import dev.anarchy.ui.control.Workspace;
 import dev.anarchy.ui.util.LaunchHelper;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Tab;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -28,6 +35,8 @@ public class ServiceChainerApp extends Application {
 	private Workspace workspace;
 	
 	private Stage stage;
+	
+	private boolean cancelDeletes;
 	
 	@Override
 	public void start(Stage stage) {
@@ -55,19 +64,42 @@ public class ServiceChainerApp extends Application {
 		
 		// Delete event. TODO clean this up.
 		DApp.get().getOnDeleteEvent().connect((args) -> {
+			if ( cancelDeletes )
+				return;
+			
 			DCollection collection = getData().getCollection((DFolderElement) args[0]);
 			
 			if ( args[0] instanceof DCollection ) {
+				AtomicBoolean deleteFromDisk = new AtomicBoolean(false);
+				ButtonType remove = requestRemoveFile(((DCollection) args[0]).getName(), deleteFromDisk);
+				if ( remove == ButtonType.CANCEL ) {
+					cancelDeletes = true;
+					Platform.runLater(()->{
+						cancelDeletes = false;
+					});
+					return;
+				}
+				
 				// Remove it from list
 				data.removeCollection(((DCollection)args[0]));
 				
 				// Delete it from disk maybe
-				boolean deleteFromDisk = requestDeleteFile(((DCollection) args[0]).getName());
-				if ( deleteFromDisk ) {
+				if ( deleteFromDisk.get() ) {
 					this.getData().deleteCollection((DCollection) args[0]);
 				}
 			}else if ( args[0] instanceof DFolderElement ) { 
 				DFolder parentNode = getData().getParent((DFolderElement) args[0]);
+				
+				AtomicBoolean deleteFromDisk = new AtomicBoolean(false);
+				ButtonType remove = requestRemoveFile(((DFolderElement) args[0]).getName(), deleteFromDisk);
+				if ( remove == ButtonType.CANCEL ) {
+					cancelDeletes = true;
+					Platform.runLater(()->{
+						cancelDeletes = false;
+					});
+					return;
+				}
+				
 				
 				// Attempt to close
 				if ( args[0] instanceof DServiceChain ) {
@@ -78,8 +110,7 @@ public class ServiceChainerApp extends Application {
 					}
 					
 					// Delete from disk
-					boolean deleteChain = requestDeleteFile(((DServiceChain) args[0]).getName());
-					if ( deleteChain ) {
+					if ( deleteFromDisk.get() ) {
 						this.getData().deleteServiceChain((DServiceChain) args[0]);
 					}
 				}
@@ -198,13 +229,52 @@ public class ServiceChainerApp extends Application {
 		return result;
 	}
 	
-	private boolean requestDeleteFile(String name) {
-		Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + name + " from disk?", ButtonType.YES, ButtonType.NO);
+	private ButtonType requestRemoveFile(String name, AtomicBoolean deleteFromDisk) {
+		/*Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete " + name + " from disk?", ButtonType.YES, ButtonType.NO);
 		ServiceChainerUIBuilder.setTheme(alert.getDialogPane());
 		alert.getDialogPane().getStylesheets().addAll(ServiceChainerUIBuilder.getStylesheet());
 		ButtonType result = alert.showAndWait().orElse(ButtonType.NO);
 		
-		return result == ButtonType.YES;
+		return result == ButtonType.YES;*/
+		
+		// Create alert
+		Alert alert = createAlertWithOptOut(AlertType.CONFIRMATION, "Delete", null, 
+				"Are you sure you wish to remove " + name + "?", "Delete file contents from disk (cannot be undone)", 
+				param -> deleteFromDisk.set(param), ButtonType.YES, ButtonType.CANCEL);
+		
+		// Get Result
+		ButtonType result = alert.showAndWait().orElse(ButtonType.NO);
+		return result;
+	}
+	
+	public static Alert createAlertWithOptOut(AlertType type, String title, String headerText, String message, String optOutMessage, Consumer<Boolean> optOutAction, ButtonType... buttonTypes) {
+		Alert alert = new Alert(type);
+		// Need to force the alert to layout in order to grab the graphic,
+		// as we are replacing the dialog pane with a custom pane
+		alert.getDialogPane().applyCss();
+		Node graphic = alert.getDialogPane().getGraphic();
+		// Create a new dialog pane that has a checkbox instead of the hide/show details button
+		// Use the supplied callback for the action of the checkbox
+		alert.setDialogPane(new DialogPane() {
+			@Override
+			protected Node createDetailsButton() {
+				CheckBox optOut = new CheckBox();
+				optOut.setText(optOutMessage);
+				optOut.setOnAction(e -> optOutAction.accept(optOut.isSelected()));
+				return optOut;
+			}
+		});
+		alert.getDialogPane().getButtonTypes().addAll(buttonTypes);
+		alert.getDialogPane().setContentText(message);
+		// Fool the dialog into thinking there is some expandable content
+		// a Group won't take up any space if it has no children
+		alert.getDialogPane().setExpandableContent(new Group());
+		alert.getDialogPane().setExpanded(true);
+		// Reset the dialog graphic using the default style
+		alert.getDialogPane().setGraphic(graphic);
+		alert.setTitle(title);
+		alert.setHeaderText(headerText);
+		return alert;
 	}
 
 	/**
